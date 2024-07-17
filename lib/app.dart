eab95dcf3033e222c2fb3d89a2ba116d484d736e
Matt Dunn <mattdunn0816@gmail.com>
2024-07-17 10:06:55 -0600

    feat(theme): added ui_extension

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_template_core/core/extensions/ui_extensions.dart';
+import 'package:flutter_template_core/core/theme/theme.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      darkTheme: AppTheme.light,
+      themeMode: ThemeMode.light,
       home: Scaffold(
+        backgroundColor: context.colors.backgroundPrimary,
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(
+            'Hello World!',
+            style: context.typography.headlineLarge.copyWith(
+              color: context.colors.foregroundPrimary,
+            ),
+          ),
         ),
       ),
     );
