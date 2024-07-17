fc71bb7ad7bb4d4ac11575381a9da1318f36e59d
Matt Dunn <mattdunn0816@gmail.com>
2024-07-17 13:38:25 -0600

    fix: updated spelling and workflow

--- lib/core/config/objectbox.dart
+++ lib/core/config/objectbox.dart
@@ -4,14 +4,14 @@ import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 
 class ObjectBox {
-  /// The Store of this app.
-  late final Store store;
-
   ObjectBox._create(this.store) {
     if (store.box<Post>().isEmpty()) {
       _putDemoData();
     }
   }
+
+  /// The Store of this app.
+  late final Store store;
 
   /// Create an instance of ObjectBox to use throughout the app.
   static Future<ObjectBox> create() async {
