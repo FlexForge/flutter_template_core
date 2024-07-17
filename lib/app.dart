7cac0975a188768cea534007392a88402c46a124
Matt Dunn <mattdunn0816@gmail.com>
2024-07-17 14:04:00 -0600

    feat(theme):  added dark and light theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_template_core/core/config/router.dart';
+import 'package:flutter_template_core/core/theme/theme.dart';
 import 'package:flutter_template_core/flavors.dart';
 
 class MainApp extends StatelessWidget {
@@ -9,6 +10,8 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: F.title,
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
       debugShowCheckedModeBanner: false,
       routeInformationParser: router.routeInformationParser,
       routerDelegate: router.routerDelegate,
