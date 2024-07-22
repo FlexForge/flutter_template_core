0786ba13f98f47b1f33b9fba112e0fdd56112a6f
Matt Dunn <mattdunn0816@gmail.com>
2024-07-21 22:59:56 -0600

    feat: added theme mode to application

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_template_core/core/config/router.dart';
 import 'package:flutter_template_core/core/theme/theme.dart';
+import 'package:flutter_template_core/features/user/controllers/theme_controller.dart';
 import 'package:flutter_template_core/flavors.dart';
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(themeControllerProvider);
+
     return MaterialApp.router(
       title: F.title,
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
+      themeMode: theme,
       debugShowCheckedModeBanner: false,
       routeInformationParser: router.routeInformationParser,
       routerDelegate: router.routerDelegate,
