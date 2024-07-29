6a4c10174ad16f056a9b0c3b74aa2b29365e0a52
Matt Dunn <mattdunn0816@gmail.com>
2024-07-29 14:27:41 -0600

    test: added onboarding_controller test

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,4 +1,7 @@
+import 'package:flutter_template_core/features/auth/data/repositories/onboarding_repository.dart';
 import 'package:flutter_template_core/features/posts/data/repositories/post_repository.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockPostRepository extends Mock implements PostRepository {}
+
+class MockOnboardingRepository extends Mock implements OnboardingRepository {}
